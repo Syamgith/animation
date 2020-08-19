0eb1a1fdec1cfdfc304899f4244bcea10ed75d76
syamgith <syamgithksudheesh@gmail.com>
2020-08-19 21:04:17 +0530

    made animation controller, animation and tween

--- lib/src/screens/home.dart
+++ lib/src/screens/home.dart
@@ -6,7 +6,24 @@ class Home extends StatefulWidget {
   _HomeState createState() => _HomeState();
 }
 
-class _HomeState extends State<Home> {
+class _HomeState extends State<Home> with TickerProviderStateMixin {
+  Animation<double> catAnimation;
+  AnimationController catController;
+
+  @override
+  void initState() {
+    super.initState();
+    catController =
+        AnimationController(duration: Duration(seconds: 2), vsync: this);
+
+    catAnimation = Tween(begin: 0.0, end: 100.0).animate(
+      CurvedAnimation(
+        parent: catController,
+        curve: Curves.easeIn,
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
